4457878b8198bafe286e68f9c68239ae48a802e3
rikeardi <risto.lievonen@gmail.com>
2022-03-26 22:32:46 +0200

    Ensimmäinen pohja

--- app/tilkku/tilkku/settings.py
+++ app/tilkku/tilkku/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static/',
 ]
 
+STATIC_URL = 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
