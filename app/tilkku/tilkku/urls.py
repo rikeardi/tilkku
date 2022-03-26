cbb8fc1d552e106437ebed9a5c59a962958120af
rikeardi <risto.lievonen@gmail.com>
2022-03-26 19:06:07 +0200

    Ensimmäinen pohja

--- app/tilkku/tilkku/urls.py
+++ app/tilkku/tilkku/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='front.html'), name='home')
+    path('', login_required(TemplateView.as_view(template_name='front.html')), name='home')
 ]
