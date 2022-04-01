5814e3a4c4f4a241d103e9a314ffa31c04ad4f19
rikeardi <risto.lievonen@gmail.com>
2022-04-01 15:19:31 +0300

    Ensimmäinen pohja

--- app/tilkku/chat/models.py
+++ app/tilkku/chat/models.py
@@ -1,10 +1,2 @@
 from django.db import models
 
-
-# Create your models here.
-@python_2_unicode_compatible
-class Room(models.Model):
-    title = models.CharField(max_length=255)
-
-    def str(self):
-        return self.title
