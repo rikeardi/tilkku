f44bb1acefd53516c1be456d7708e510c39f2b67
rikeardi <risto.lievonen@gmail.com>
2022-04-02 11:33:56 +0300

    Ensimmäinen pohja

--- app/tilkku/tilkku/asgi.py
+++ app/tilkku/tilkku/asgi.py
@@ -9,11 +9,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tilkku.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
 })
