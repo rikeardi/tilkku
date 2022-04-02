81164898769c52d1d9cf91ec3a1a14745093bd1a
rikeardi <risto.lievonen@gmail.com>
2022-04-02 14:49:41 +0300

    Ensimmäinen pohja

--- runserver.sh
+++ runserver.sh
@@ -2,6 +2,7 @@
 
 cd /code/app/tilkku
 
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py createsuperuser --noinput
 python manage.py runserver 0.0.0.0:8000
