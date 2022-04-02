ddd9add9f37d7a83bf9d4a729481cbe6c424529f
rikeardi <risto.lievonen@gmail.com>
2022-04-02 12:22:43 +0300

    Ensimmäinen pohja

--- app/tilkku/chat/consumers.py
+++ app/tilkku/chat/consumers.py
@@ -24,19 +24,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None, blob_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        sender = text_data_json['sender']
+        user = text_data_json['user']
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'sender': sender,
+                'user': user
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
+        sender = event['sender']
+        user = event['user']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'sender': sender,
+            'user': user
         }))
