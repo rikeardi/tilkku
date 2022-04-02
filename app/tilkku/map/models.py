ffb9c70008892ba50ae12fc50a250df83d0cb8d4
rikeardi <risto.lievonen@gmail.com>
2022-04-02 14:39:08 +0300

    Ensimmäinen pohja

--- app/tilkku/map/models.py
+++ app/tilkku/map/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class LayerType(models.TextChoices):
+    AREA = 'AR', 'Alue'
+    MARKER = 'MA', 'Merkintä'
+
+
+class MapStyle(models.Model):
+    stroke = models.CharField(max_length=10)
+    fill = models.CharField(max_length=10)
+    opacity = models.FloatField()
+
+
+class Layer(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=2, choices=LayerType.choices, default=LayerType.AREA)
+    style = models.ForeignKey(MapStyle, on_delete=models.DO_NOTHING)
+
+
+class Area(models.Model):
+    name = models.CharField(max_length=200)
+    layer = models.ForeignKey(Layer, on_delete=models.DO_NOTHING)
+    coordinates = models.TextField()
+
+
+class Marker(models.Model):
+    name = models.CharField(max_length=200)
+    layer = models.ForeignKey(Layer, on_delete=models.DO_NOTHING)
+    coordinates = models.TextField()
