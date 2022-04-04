92de6538dd68f3727243e9917f7f37d495e4238a
rikeardi <risto.lievonen@gmail.com>
2022-04-04 20:48:06 +0300

    Ensimmäinen pohja

--- app/tilkku/chat/consumers.py
+++ app/tilkku/chat/consumers.py
@@ -17,7 +17,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        self.room = Room.objects.get(name=self.room_name)
+        self.room = sync_to_async(Room.objects.get)(name=self.room_name)
         self.user = self.scope['user']
 
         # Join room group
