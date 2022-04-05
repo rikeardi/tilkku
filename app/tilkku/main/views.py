e81c9bb024d276450528d4b930a2bdd65d74178f
rikeardi <risto.lievonen@gmail.com>
2022-04-05 21:50:19 +0300

    Ensimmäinen pohja

--- app/tilkku/main/views.py
+++ app/tilkku/main/views.py
@@ -1,5 +1,4 @@
-import json
-
+from django.core import serializers
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -8,8 +7,8 @@ from map.models import MapServer
 
 @login_required
 def home(request):
-    mapservers = list(MapServer.objects.all())
+    mapservers = MapServer.objects.all()
     context = {
-        "map_servers": mapservers
+        "map_servers": serializers.serialize('json', mapservers)
     }
     return render(request, "front.html", context)
