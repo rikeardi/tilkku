de4710c1e0a2dd256a010d465a46ca6d5a4c3c1e
rikeardi <risto.lievonen@gmail.com>
2022-04-05 21:19:14 +0300

    Ensimmäinen pohja

--- app/tilkku/main/views.py
+++ app/tilkku/main/views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
-# Create your views here.
-from app.tilkku.map.models import MapServer
+from map.models import MapServer
 
 
 @login_required
