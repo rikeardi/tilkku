58d91e2ead616097c5c5e65cf29add66bdc16cc8
rikeardi <risto.lievonen@gmail.com>
2022-04-05 22:26:47 +0300

    Ensimmäinen pohja

--- app/tilkku/main/models.py
+++ app/tilkku/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.encoding import force_text
+from django.core.serializers.json import DjangoJSONEncoder
 
-# Create your models here.
+
+class LazyEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+#        if isinstance(obj, YourCustomType):
+#            return force_text(obj)
+        return super(LazyEncoder, self).default(obj)
--- app/tilkku/main/views.py
+++ app/tilkku/main/views.py
@@ -3,11 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from map.models import MapServer
+from .models import LazyEncoder
 
 
 @login_required
 def home(request):
     context = {
-        "map_servers": serializers.serialize('json', MapServer.objects.all(), fields=('name', 'url', 'attribution'))
+        "map_servers": serializers.serialize('json', MapServer.objects.all(), cls=LazyEncoder)
     }
     return render(request, "front.html", context)
