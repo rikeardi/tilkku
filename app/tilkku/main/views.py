3ddc06f2fe353ebd85ff0aa42b77b7f1d54cc127
rikeardi <risto.lievonen@gmail.com>
2022-04-06 22:09:45 +0300

    Ensimmäinen pohja

--- app/tilkku/main/views.py
+++ app/tilkku/main/views.py
@@ -8,6 +8,6 @@ from map.models import MapServer
 @login_required
 def home(request):
     context = {
-        "map_servers": MapServer.objects.all()
+        "map_servers": list(MapServer.objects.all())
     }
     return render(request, "front.html", context)
