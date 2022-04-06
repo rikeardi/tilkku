cd7453a4fb4f861f9d1a0a12fd5f94aa0418d3d2
rikeardi <risto.lievonen@gmail.com>
2022-04-06 22:05:08 +0300

    Ensimmäinen pohja

--- app/tilkku/main/views.py
+++ app/tilkku/main/views.py
@@ -3,12 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_list_or_404
 
 from map.models import MapServer
-from .models import LazyEncoder
 
 
 @login_required
 def home(request):
     context = {
-        "map_servers": get_list_or_404(MapServer.objects.all())
+        "map_servers": serialize(MapServer.objects.all())
     }
     return render(request, "front.html", context)
