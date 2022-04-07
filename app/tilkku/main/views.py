9ba7e5d2cffe6e8fe002762dfdd24979ebc9de67
rikeardi <risto.lievonen@gmail.com>
2022-04-07 22:03:24 +0300

    Ensimmäinen pohja

--- app/tilkku/main/views.py
+++ app/tilkku/main/views.py
@@ -5,14 +5,16 @@ from django.shortcuts import render
 from map.models import *
 
 
+# Create view for the main page
+# add login_required decorator to prevent unauthenticated users from accessing the page
+# add a context variable to pass list of MapServer objects to the template
+# add a context variable to pass list of MapLayer objects to the template with all subobjects (Area, Marker, etc.)
+# template: front.html
+# name: home
 @login_required
 def home(request):
-    layers = Layer.objects.select_related()
-    areas = Area.objects.select_related()
-
     context = {
-        "map_servers": serialize('json', MapServer.objects.all()),
-        "layers": serialize('json', layers),
-        "areas": serialize('json', areas),
+        'map_servers': serialize('json', MapServer.objects.all()),
+        'layers': serialize('json', Layer.objects.all(), fields=('id', 'name', 'area_list', 'marker_list')),
     }
-    return render(request, "front.html", context)
+    return render(request, 'front.html', context)
