e693aea05443df59e3f7d69cd1bb0dfaf54ee8ce
rikeardi <risto.lievonen@gmail.com>
2022-04-07 22:25:05 +0300

    Ensimmäinen pohja

--- app/tilkku/map/models.py
+++ app/tilkku/map/models.py
@@ -7,11 +7,17 @@ class LayerType(models.TextChoices):
     MARKER = 'MA', 'Merkintä'
 
 
+class MapStyleManager(models.Manager):
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
+
 class MapStyle(models.Model):
     name = models.CharField(max_length=50)
     stroke = models.CharField(max_length=10)
     fill = models.CharField(max_length=10)
     opacity = models.FloatField()
+    objects = MapStyleManager()
 
     def __str__(self):
         return f'{self.name}'
@@ -21,24 +27,37 @@ class Layer(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=2, choices=LayerType.choices, default=LayerType.AREA)
     style = models.ForeignKey(MapStyle, on_delete=models.DO_NOTHING)
+    objects = LayerManager()
 
     def __str__(self):
         return f'{self.name}'
+
+
+class AreaManager(models.Manager):
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
 
 
 class Area(models.Model):
     name = models.CharField(max_length=200)
     layer = models.ForeignKey(Layer, related_name="areas", on_delete=models.DO_NOTHING)
     coordinates = models.TextField()
+    objects = AreaManager()
 
     def __str__(self):
         return f'{self.name}'
+
+
+class MarkerManager(models.Manager):
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
 
 
 class Marker(models.Model):
     name = models.CharField(max_length=200)
     layer = models.ForeignKey(Layer, related_name="markers", on_delete=models.DO_NOTHING)
     coordinates = models.TextField()
+    objects = MarkerManager()
 
     def __str__(self):
         return f'{self.name}'
