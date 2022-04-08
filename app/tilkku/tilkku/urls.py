36b5eb83001ae65c7c9ebec980f381010138a0cf
rikeardi <risto.lievonen@gmail.com>
2022-04-08 15:25:05 +0300

    Ensimmäinen pohja

--- app/tilkku/tilkku/urls.py
+++ app/tilkku/tilkku/urls.py
@@ -18,14 +18,13 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework import routers
-from main import views
 
 router = routers.DefaultRouter()
-router.register(r'', views.Home)
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('main.urls')),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
