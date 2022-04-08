b4a903d5d7ff3e06040aef784bcaceac6c3f5be4
rikeardi <risto.lievonen@gmail.com>
2022-04-08 14:43:37 +0300

    Ensimmäinen pohja

--- app/tilkku/map/serializers.py
+++ app/tilkku/map/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import *
 
 
-class LayerSerializer(serializers.HyperlinkedModelSerializer):
+class LayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Layer
         fields = ['id', 'name', 'type', 'style', 'areas', 'markers']
