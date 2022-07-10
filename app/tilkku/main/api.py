4c2b659027c067965643c6801185e00a74c0a938
rikeardi <risto.lievonen@gmail.com>
2022-07-10 08:53:56 +0300

    Ensimmäinen pohja

---
+++ app/tilkku/main/api.py
@@ -0,0 +1,62 @@
+import json
+from datetime import datetime
+
+from django.db.models import Q
+from rest_framework import serializers, viewsets, generics
+from rest_framework.response import Response
+from .models import *
+
+
+class MapStyleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MapStyle
+        fields = ('id', 'name', 'stroke', 'fill', 'opacity')
+
+
+class MapStyleViewSet(viewsets.ModelViewSet):
+    queryset = MapStyle.objects.all()
+    serializer_class = MapStyleSerializer
+
+
+class LayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Layer
+        fields = ('id', 'name', 'type', 'style')
+
+
+class LayerViewSet(viewsets.ModelViewSet):
+    queryset = Layer.objects.all()
+    serializer_class = LayerSerializer
+
+
+class AreaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Area
+        fields = ('id', 'name', 'subtitle', 'layer', 'coordinates')
+
+
+class AreaViewSet(viewsets.ModelViewSet):
+    queryset = Area.objects.all()
+    serializer_class = AreaSerializer
+
+
+class SiteCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SiteCategory
+        fields = ('id', 'name')
+
+
+class SiteCategoryViewSet(viewsets.ModelViewSet):
+    queryset = SiteCategory.objects.all()
+    serializer_class = SiteCategorySerializer
+
+
+class SiteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Site
+        fields = ('id', 'name', 'area', 'marker', 'category', 'description')
+
+
+class SiteViewSet(viewsets.ModelViewSet):
+    queryset = Site.objects.all()
+    serializer_class = SiteSerializer
--- app/tilkku/main/urls.py
+++ app/tilkku/main/urls.py
@@ -1,13 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 import django.contrib.auth.views
+from rest_framework import routers
 from django.views.generic import TemplateView
 
-from . import views, forms
+from . import views, forms, api
+
+router = routers.DefaultRouter()
+router.register(r'mapstyles', api.MapStyleViewSet)
+router.register(r'layers', api.LayerViewSet)
+router.register(r'areas', api.AreaViewSet)
+router.register(r'sitecategories', api.SiteCategoryViewSet)
+router.register(r'sites', api.SiteViewSet)
 
 app_name = 'main'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('accounts/login/', django.contrib.auth.views.LoginView.as_view(authentication_form=forms.LoginForm), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
