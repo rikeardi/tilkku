90e242ff36f02dd692a95688cbebbb482f33a84d
rikeardi <risto.lievonen@gmail.com>
2022-07-12 10:42:21 +0300

    Ensimmäinen pohja

--- app/tilkku/map/models.py
+++ app/tilkku/map/models.py
@@ -105,7 +105,7 @@ class Site(models.Model):
     category = models.ForeignKey(SiteCategory, on_delete=models.DO_NOTHING)
     description = models.TextField(blank=True, default='')
     contacts = models.ManyToManyField(Contact, blank=True)
-    notes = models.ManyToManyField('tilkku.Note', blank=True)
+    notes = models.ManyToManyField('Note', blank=True)
 
     class Meta:
         ordering = ['name']
