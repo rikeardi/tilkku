44307f9a427a434822022f1f1d9310723776887f
rikeardi <risto.lievonen@gmail.com>
2022-07-12 14:51:47 +0300

    Ensimmäinen pohja

--- app/tilkku/main/api.py
+++ app/tilkku/main/api.py
@@ -18,6 +18,13 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
 
 class MapStyleSerializer(serializers.ModelSerializer):
     class Meta:
