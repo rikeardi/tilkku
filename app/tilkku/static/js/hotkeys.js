4ef2719177091328421e9759b9d8321d5ef5b7e9
rikeardi <risto.lievonen@gmail.com>
2022-07-17 18:23:17 +0300

    Ensimmäinen pohja

--- app/tilkku/static/js/hotkeys.js
+++ app/tilkku/static/js/hotkeys.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
         if($("input").is(":focus") || $("textarea").is(":focus")) {
             return;
         }
-        console.log(e.keyCode);
+
+        if(e.keyCode == 65) {
+            new_topic_modal_open();
+        }
     });
 });
