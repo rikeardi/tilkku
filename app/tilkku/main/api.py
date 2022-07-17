da79d364b0fb370acd9bd6ef9a4d1d93f9f579c4
rikeardi <risto.lievonen@gmail.com>
2022-07-17 16:06:08 +0300

    Ensimmäinen pohja

--- app/tilkku/main/api.py
+++ app/tilkku/main/api.py
@@ -314,7 +314,6 @@ class GeoJSONViewSet(generics.RetrieveAPIView):
     class Meta:
         model = GeoJSON
         fields = ('type', 'features')
-        basename = 'geojson'
 
     def get_queryset(self):
         queryset = GeoJSON()
