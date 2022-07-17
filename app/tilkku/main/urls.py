b2b1bb6c8d7a29fb5ea67606321733f258b4d81b
rikeardi <risto.lievonen@gmail.com>
2022-07-17 16:14:16 +0300

    Ensimmäinen pohja

--- app/tilkku/main/urls.py
+++ app/tilkku/main/urls.py
@@ -17,7 +17,7 @@ router.register(r'users', api.UserViewSet)
 router.register(r'notes', api.NoteViewSet)
 router.register(r'topics', api.TopicViewSet)
 router.register(r'contacts', api.ContactViewSet)
-router.register(r'geojson', api.GeoJSONViewSet, basename='geojson')
+router.register(r'geojson', api.GeoJSONViewSet.as_view(), basename='geojson')
 
 app_name = 'main'
 urlpatterns = [
