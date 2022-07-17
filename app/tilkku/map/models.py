f73514099110e57505242968057f58dc56be9a8b
rikeardi <risto.lievonen@gmail.com>
2022-07-17 15:30:05 +0300

    Ensimmäinen pohja

--- app/tilkku/map/models.py
+++ app/tilkku/map/models.py
@@ -50,6 +50,7 @@ class GeoItem(models.Model):
 
 class Area(GeoItem):
     subtitle = models.CharField(max_length=300, blank=True, default='')
+    type = models.CharField(max_length=2, default='AR')
 
     class Meta:
         ordering = ['name']
@@ -59,6 +60,8 @@ class Area(GeoItem):
 
 
 class Marker(GeoItem):
+    type = models.CharField(max_length=2, default='PO')
+
     class Meta:
         ordering = ['name']
 
