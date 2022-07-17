4abd8ecdeef2e604bf48f4300fb77d34285de1c4
rikeardi <risto.lievonen@gmail.com>
2022-07-17 16:59:04 +0300

    Ensimmäinen pohja

--- app/tilkku/main/api.py
+++ app/tilkku/main/api.py
@@ -322,7 +322,7 @@ class GeoJSONViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
         instance = self.get_object()
         areas = Area.objects.all()
         markers = Marker.objects.all()
-        instance.features = areas
+        instance.features = list(chain(areas, markers))
         return Response(GeoJSONSerializer(instance).data)
 
     def get_object(self):
