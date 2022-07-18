76d2fcce241cb089f75d71839a837858a7c28d74
rikeardi <risto.lievonen@gmail.com>
2022-07-18 10:55:03 +0300

    Localization

--- app/tilkku/tilkku/context_processor.py
+++ app/tilkku/tilkku/context_processor.py
@@ -1,6 +1,9 @@
 import os
 
+
 def export_env_variables(request):
     return {
-        'HOME_LAT': os.environ['HOME_LAT'],
+        'environment': {
+            'HOME_LAT': os.environ['HOME_LAT'],
+        }
     }
