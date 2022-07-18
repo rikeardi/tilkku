f585aed247934c146828e0d039131e5b181df562
rikeardi <risto.lievonen@gmail.com>
2022-07-18 10:28:43 +0300

    Localization

--- app/tilkku/static/js/hotkeys.js
+++ app/tilkku/static/js/hotkeys.js
@@ -7,42 +7,42 @@ $(document).ready(function() {
 
         console.log(e.keyCode);
 
-        if(e.keyCode == 97) {
+        if(e.keyCode == 101) { // e
             e.preventDefault();
             toolbox_tool('events');
             new_topic_modal_open();
         }
 
-        if(e.keyCode == 112) {
+        if(e.keyCode == 108) { // l
             e.preventDefault();
             toolbox_tool('sites');
             $("#sites-site-search").focus();
         }
 
-        if(e.keyCode == 121) {
+        if(e.keyCode == 112) { // p
             e.preventDefault();
             toolbox_tool('contacts');
             $("#contacts-search").focus();
         }
 
-        if(e.keyCode == 25) {
+        if(e.keyCode == 25) { // ctrl+y
             e.preventDefault();
             toolbox_tool('contacts');
             new_contact_modal_open();
         }
 
-        if(e.keyCode == 99) {
+        if(e.keyCode == 99) { // c
             e.preventDefault();
             toolbox_tool('chat');
             $("#chat-input").focus();
         }
 
-        if(e.keyCode == 109) {
+        if(e.keyCode == 109) { // m
             e.preventDefault();
             toolbox_menu();
         }
 
-        if(e.keyCode == 115) {
+        if(e.keyCode == 115) { // s
             e.preventDefault();
             settings_open();
         }
