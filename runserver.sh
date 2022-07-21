8c15dae90cb4278255a8b7c7a08d862b123b53e1
rikeardi <risto.lievonen@gmail.com>
2022-07-21 10:00:44 +0300

    Localization

--- runserver.sh
+++ runserver.sh
@@ -9,5 +9,5 @@ python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput
 python manage.py loaddata tilkku/fixtures.yaml
 
-#python manage.py runserver 0.0.0.0:8000
-gunicorn tilkku.asgi --bind 0.0.0.0:8000 --workers 4
+python manage.py runserver 0.0.0.0:8000
+#gunicorn tilkku.asgi --bind 0.0.0.0:8000 --workers 4
